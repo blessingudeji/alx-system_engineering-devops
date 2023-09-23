72c12f5a6c4230b92233f96bf7c4b9f9d5424134
Blessing Udeji <budeji@gmail.com>
2023-09-23 03:24:46 +0100

    configuration management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creates a puppet file
+file { '/tmp/school':
+    ensure  => 'file',
+	path    => '/tmp/school',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
